23a1cd4edb8ff29b8dbebd8ec1b18ed24575877c
Alex <admin@gmail.com>
2024-06-23 10:51:57 +0300

    render first 12 cart for index.html

---
+++ js/config.js
@@ -0,0 +1,4 @@
+const SERVER_NAME = 'localhost';
+const PROTOCOL = 'http://'
+const PORT = 3001
+const APP_URL = PROTOCOL+SERVER_NAME+':'+PORT;
--- js/products.js
+++ js/products.js
@@ -1,391 +1,68 @@
-let Products = [
-    {
-        id: 1,
-        img: '1.jpg',
-        price: 135000,
-        discount: "-10%",
-        productname: "Iphone1",
-        articul: "b135"
-    },
-    {
-        id: 2,
-        img: '2.png',
-        price: 125000,
-        discount: "-5%",
-        productname: "Iphone2",
-        articul: "b136"
-    },
-    {
-        id: 3,
-        img: '3.jpg',
-        price: 105000,
-        discount: "-3%",
-        productname: "Iphone3",
-        articul: "b137"
-    },
-    {
-        id: 4,
-        img: '4.jpg',
-        price: 125000,
-        discount: "-7%",
-        productname: "Iphone4",
-        articul: "b138"
-    },
-    {
-        id: 5,
-        img: '5.jpg',
-        price: 15000,
-        discount: "-15%",
-        productname: "Iphone5",
-        articul: "b139"
-    },
-    {
-        id: 6,
-        img: '6.jpg',
-        price: 13000,
-        discount: "-2%",
-        productname: "Iphone6",
-        articul: "b140"
-    },
-    {
-        id: 7,
-        img: '7.jpg',
-        price: 12000,
-        discount: "-12%",
-        productname: "Iphone7",
-        articul: "b141"
-    },
-    {
-        id: 8,
-        img: '8.png',
-        price: 16300,
-        discount: "-20%",
-        productname: "Iphone8",
-        articul: "b142"
-    },
-    {
-        id: 9,
-        img: '9.jpg',
-        price: 16000,
-        discount: "-10%",
-        productname: "Iphone9",
-        articul: "b143"
-    },
-    {
-        id: 10,
-        img: '10.jpg',
-        price: 163000,
-        discount: "-13%",
-        productname: "Iphone10",
-        articul: "b144"
-    },
-    {
-        id: 11,
-        img: '11.jpg',
-        price: 112000,
-        discount: "-13%",
-        productname: "Iphone11",
-        articul: "b145"
-    },
-    {
-        id: 12,
-        img: '12.jpg',
-        price: 1345000,
-        discount: "-12%",
-        productname: "Iphone12",
-        articul: "b146"
-    },
-    {
-        id: 13,
-        img: '13.jpg',
-        price: 123500,
-        discount: "-11%",
-        productname: "Iphone13",
-        articul: "b147"
-    },
-    {
-        id: 14,
-        img: '14.jpg',
-        price: 169840,
-        discount: "-9%",
-        productname: "Iphone14",
-        articul: "b148"
-    },
-    {
-        id: 15,
-        img: '15.jpg',
-        price: 51623,
-        discount: "-14%",
-        productname: "Iphone15",
-        articul: "b149"
-    },
-    {
-        id: 16,
-        img: '16.jpg',
-        price: 23521,
-        discount: "-9%",
-        productname: "Iphone16",
-        articul: "b150"
-    },
-    {
-        id: 17,
-        img: '17.jpg',
-        price: 81623,
-        discount: "-13%",
-        productname: "Iphone17",
-        articul: "b151"
-    },
-    {
-        id: 18,
-        img: '18.png',
-        price: 182569,
-        discount: "-15%",
-        productname: "Iphone18",
-        articul: "b152"
-    },
-    {
-        id: 19,
-        img: '19.jpg',
-        price: 928754,
-        discount: "-10%",
-        productname: "Iphone19",
-        articul: "b153"
-    },
-    {
-        id: 20,
-        img: '20.jpg',
-        price: 98263,
-        discount: "-10%",
-        productname: "Iphone20",
-        articul: "b154"
-    },
-    {
-        id: 21,
-        img: '21.jpg',
-        price: 98236,
-        discount: "-12%",
-        productname: "Iphone21",
-        articul: "b155"
-    },
-    {
-        id: 22,
-        img: '22.jpg',
-        price: 65675,
-        discount: "-13%",
-        productname: "Iphone22",
-        articul: "b156"
-    },
-    {
-        id: 23,
-        img: '23.jpg',
-        price: 92865,
-        discount: "-14%",
-        productname: "Iphone23",
-        articul: "b157"
-    },
-    {
-        id: 24,
-        img: '24.jpg',
-        price: 917654,
-        discount: "-12%",
-        productname: "Iphone24",
-        articul: "b158"
-    },
-    {
-        id: 25,
-        img: '25.jpg',
-        price: 182659,
-        discount: "-16%",
-        productname: "Iphone25",
-        articul: "b159"
-    },
-    {
-        id: 26,
-        img: '26.jpg',
-        price: 1245125,
-        discount: "-25%",
-        productname: "Iphone26",
-        articul: "b160"
-    },
-    {
-        id: 27,
-        img: '27.jpg',
-        price: 98632,
-        discount: "-13%",
-        productname: "Iphone27",
-        articul: "b161"
-    },
-    {
-        id: 28,
-        img: '28.jpg',
-        price: 91524,
-        discount: "-8%",
-        productname: "Iphone28",
-        articul: "b162"
-    },
-    {
-        id: 29,
-        img: '29.png',
-        price: 873543,
-        discount: "-15%",
-        productname: "Iphone29",
-        articul: "b163"
-    },
-    {
-        id: 30,
-        img: '30.png',
-        price: 81544,
-        discount: "-18%",
-        productname: "Iphone30",
-        articul: "b164"
-    },
-    {
-        id: 31,
-        img: '31.jpg',
-        price: 23426,
-        discount: "-10%",
-        productname: "Iphone31",
-        articul: "b165"
-    },
-    {
-        id: 32,
-        img: '32.jpg',
-        price: 232626,
-        discount: "-11%",
-        productname: "Iphone32",
-        articul: "b166"
-    },
-    {
-        id: 33,
-        img: '33.jpg',
-        price: 182754,
-        discount: "-17%",
-        productname: "Iphone33",
-        articul: "b167"
-    },
-    {
-        id: 34,
-        img: '34.jpg',
-        price: 283745,
-        discount: "-19%",
-        productname: "Iphone34",
-        articul: "b168"
-    },
-    {
-        id: 35,
-        img: '35.jpg',
-        price: 98273,
-        discount: "-12%",
-        productname: "Iphone35",
-        articul: "b169"
-    },
-    {
-        id: 36,
-        img: '36.jpg',
-        price: 2342355,
-        discount: "-15%",
-        productname: "Iphone36",
-        articul: "b170"
-    },
-    {
-        id: 37,
-        img: '37.jpg',
-        price: 92865,
-        discount: "-14%",
-        productname: "Iphone37",
-        articul: "b171"
-    },
-    {
-        id: 38,
-        img: '38.jpg',
-        price: 124156,
-        discount: "-12%",
-        productname: "Iphone38",
-        articul: "b172"
-    },
-    {
-        id: 39,
-        img: '39.jpg',
-        price: 124512,
-        discount: "-14%",
-        productname: "Iphone39",
-        articul: "b173"
-    },
-    {
-        id: 40,
-        img: '40.jpg',
-        price: 827354,
-        discount: "-13%",
-        productname: "Iphone40",
-        articul: "b174"
-    },
-];
-
-
+// при загрузке страницы получаем хранилище
+window.onload = () => {
+  fetch(APP_URL+'/products?_start=0&_limit=12')
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (products) {
+        // массив с данными
+        renderProducts(products);
+    });
+};
 
 // шаблон карточки
-const template = document.querySelector('#card').content;
+const template = document.querySelector("#card").content;
 // общий блок для карточек
-const cards = document.querySelector('.cards');
-Products.forEach(function (product, index) {
+const cards = document.querySelector(".cards");
 
-});
+function renderProducts(products) {
 
-Products.forEach(product => {
-    let card = template.querySelector('.card').cloneNode(true);
-
+  products.forEach((product) => {
+    let card = template.querySelector(".card").cloneNode(true);
     // устанавливаем id товара в атрибут data-id для карточки
     card.dataset.id = product.id;
-    // card.setAttribute('data-id',product.id);
-
-    // замена контента 
-    card.querySelector('.card__title').textContent = product.productname;
-    card.querySelector('.card__image').href = '/pages/card/index.html?id=' + product.id;
-    card.querySelector('.card__title').href = '/pages/card/index.html?id=' + product.id;
-    card.querySelector('img').src = `./src/phones/${product.img}`;
-    card.querySelector('.card__prices').textContent = product.price;
-    card.querySelector('.card__label').textContent = product.discount;
-
+    // замена контента
+    card.querySelector(".card__title").textContent = product.productname;
+    card.querySelector(".card__image").href =
+      "/pages/card/index.html?id=" + product.id;
+    card.querySelector(".card__title").href =
+      "/pages/card/index.html?id=" + product.id;
+    card.querySelector("img").src = `./src/phones/${product.img}`;
+    card.querySelector(".card__prices").textContent = product.price;
+    card.querySelector(".card__label").textContent = product.discount;
     // положили id товара в атрибут data-id у кнопки
-    card.querySelector('.card__add').dataset.id = product.id;
-    // отрисовываем карточку в общий блок 
-    cards.insertAdjacentElement('beforeend', card);
-
-    // добавляем карточку в общий блок 
-    //cards.appendChild(card);
-
-});
-
-
+    card.querySelector(".card__add").dataset.id = product.id;
+    // отрисовываем карточку в общий блок
+    cards.insertAdjacentElement("beforeend", card);
+  });
+}
 function addProduct(btn) {
-    //console.log(btn.closest('.card').getAttribute('data-id'));
-    //console.log(btn.closest('.card').dataset.id);
-    //console.log(btn.dataset.id);
-    let id = btn.dataset.id;
+  //console.log(btn.closest('.card').getAttribute('data-id'));
+  //console.log(btn.closest('.card').dataset.id);
+  //console.log(btn.dataset.id);
+  let id = btn.dataset.id;
 
-    // JSON.stringify(products); преобразования в строку в формате JSON
-    // JSON.parse(products);    преобразования из строки в обьект или массив
+  // JSON.stringify(products); преобразования в строку в формате JSON
+  // JSON.parse(products);    преобразования из строки в обьект или массив
 
-    // получаем данные из localStorage по ключу key_products
-    let storage = localStorage.getItem('key_products');
+  // получаем данные из localStorage по ключу key_products
+  let storage = localStorage.getItem("key_products");
 
-    // если есть ключ key_products в localStorage
-    if (storage) {
-        // преобразовываем строку в обьект и перезаписываем переменную storage
-        storage = JSON.parse(storage);
-    }
-    else {
-        // если нет ключа key_products в localStorage, то создаем пустой обьект
-        storage = {};
-    }
+  // если есть ключ key_products в localStorage
+  if (storage) {
+    // преобразовываем строку в обьект и перезаписываем переменную storage
+    storage = JSON.parse(storage);
+  } else {
+    // если нет ключа key_products в localStorage, то создаем пустой обьект
+    storage = {};
+  }
 
-    // если товара нет в корзине, то добавляем
-    if (!storage[id]) {
-        storage[id] = 1;
-    } else {
-        storage[id] += 1;
-    }
+  // если товара нет в корзине, то добавляем
+  if (!storage[id]) {
+    storage[id] = 1;
+  } else {
+    storage[id] += 1;
+  }
 
-    localStorage.setItem('key_products', JSON.stringify(storage));
-
+  localStorage.setItem("key_products", JSON.stringify(storage));
 }
